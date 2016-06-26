97b353809b8217ae747262bb57d49e309590ef76
Marc <marc.debureaux@sa-cim.fr>
2016-06-26 02:53:06 +0200

    Ajout des armes, armures et munitions

--- fallout/app/admin.py
+++ fallout/app/admin.py
@@ -1,3 +1,9 @@
+# coding: utf-8
 from django.contrib import admin
+from .models import Ammo, Apparel, Character, Item, Weapon
 
-# Register your models here.
+admin.site.register(Ammo)
+admin.site.register(Apparel)
+admin.site.register(Character)
+admin.site.register(Item)
+admin.site.register(Weapon)
--- fallout/app/models.py
+++ fallout/app/models.py
@@ -1,5 +1,8 @@
 # encoding: utf-8
+from typing import List
+
 from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.translation import ugettext as _
 
@@ -8,6 +11,19 @@ class Character(models.Model):
     """
     Personnage
     """
+    STAT_HEALTH = 'health'
+    STAT_ACTION_POINTS = 'action_points'
+    STAT_IRRADIATION = 'irradiation'
+    STAT_EXPERIENCE = 'experience'
+    STAT_KARMA = 'karma'
+    GENERAL_STATS = (
+        (STAT_EXPERIENCE, _("expérience")),
+        (STAT_KARMA, _("karma")),
+        (STAT_HEALTH, _("santé")),
+        (STAT_ACTION_POINTS, _("points d'action")),
+        (STAT_IRRADIATION, _("irradiation")),
+    )
+
     SPECIAL_STRENGTH = 'strength'
     SPECIAL_PERCEPTION = 'perception'
     SPECIAL_ENDURANCE = 'endurance'
@@ -70,7 +86,7 @@ class Character(models.Model):
     STAT_MAX_ACTION_POINTS = 'max_action_points'
     STAT_DAMAGE_RESISTANCE = 'damage_resistance'
     STAT_DAMAGE_THRESHOLD = 'damage_threashold'
-    STAT_ARMOR_CLASS = 'armor_class'
+    STAT_EVADE = 'evade'
     STAT_CARRY_WEIGHT = 'carry_weight'
     STAT_MELEE_DAMAGE = 'melee_damage'
     STAT_POISON_RESISTANCE = 'poison_resistance'
@@ -82,25 +98,29 @@ class Character(models.Model):
     STAT_HEALING_RATE = 'healing_rate'
     STAT_CRITICAL_CHANCE = 'critical_chance'
     STATS = (
-        (STAT_MAX_HEALTH, "santé maximale"),
-        (STAT_MAX_ACTION_POINTS, "points d'action max."),
-        (STAT_DAMAGE_RESISTANCE, "résistance aux dégâts"),
-        (STAT_DAMAGE_THRESHOLD, "seuil de dégâts"),
-        (STAT_ARMOR_CLASS, "armure"),
-        (STAT_CARRY_WEIGHT, "charge maximale"),
-        (STAT_MELEE_DAMAGE, "attaque en mélée"),
-        (STAT_POISON_RESISTANCE, "résistance aux poisons"),
-        (STAT_RADIATION_RESISTANCE, "résistance aux radiations"),
-        (STAT_GAS_RESISTANCE, "résistance aux gaz"),
-        (STAT_FIRE_RESISTANCE, "résistance au feu"),
-        (STAT_ELECTRICITY_RESISTANCE, "résistance à l'électricité"),
-        (STAT_SEQUENCE, "initiative"),
-        (STAT_HEALING_RATE, "taux de regénération"),
-        (STAT_CRITICAL_CHANCE, "chance de critique"),
+        (STAT_MAX_HEALTH, _("santé maximale")),
+        (STAT_MAX_ACTION_POINTS, _("points d'action max.")),
+        (STAT_DAMAGE_RESISTANCE, _("résistance aux dégâts")),
+        (STAT_DAMAGE_THRESHOLD, _("seuil de dégâts")),
+        (STAT_EVADE, _("esquive")),
+        (STAT_CARRY_WEIGHT, _("charge maximale")),
+        (STAT_MELEE_DAMAGE, _("attaque en mélée")),
+        (STAT_POISON_RESISTANCE, _("résistance aux poisons")),
+        (STAT_RADIATION_RESISTANCE, _("résistance aux radiations")),
+        (STAT_GAS_RESISTANCE, _("résistance aux gaz")),
+        (STAT_FIRE_RESISTANCE, _("résistance au feu")),
+        (STAT_ELECTRICITY_RESISTANCE, _("résistance à l'électricité")),
+        (STAT_SEQUENCE, _("initiative")),
+        (STAT_HEALING_RATE, _("taux de regénération")),
+        (STAT_CRITICAL_CHANCE, _("chance de critique")),
     )
+
+    # Statistics cache
+    STATISTICS = {}
 
     # General informations
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name=_("utilisateur"))
+    image = models.ImageField(blank=True, null=True, verbose_name=_("image"))
     is_player = models.BooleanField(default=False, verbose_name=_("joueur ?"))
     name = models.CharField(max_length=100, verbose_name=_("nom"))
     title = models.CharField(max_length=200, blank=True, null=True, verbose_name=_("titre"))
@@ -125,7 +145,7 @@ class Character(models.Model):
     max_action_points = models.SmallIntegerField(default=0, verbose_name=_("points d'action max."))
     damage_resistance = models.SmallIntegerField(default=0, verbose_name=_("résistance aux dégâts"))
     damage_threshold = models.SmallIntegerField(default=0, verbose_name=_("seuil de dégâts"))
-    armor_class = models.SmallIntegerField(default=0, verbose_name=_("armure"))
+    evade = models.SmallIntegerField(default=0, verbose_name=_("esquive"))
     carry_weight = models.SmallIntegerField(default=0, verbose_name=_("charge maximale"))
     melee_damage = models.SmallIntegerField(default=0, verbose_name=_("attaque en mélée"))
     poison_resistance = models.SmallIntegerField(default=0, verbose_name=_("résistance aux poisons"))
@@ -157,10 +177,11 @@ class Character(models.Model):
     gambling = models.SmallIntegerField(default=0, verbose_name=_("hasard"))
     survival = models.SmallIntegerField(default=0, verbose_name=_("survie"))
     # Tag skills
-    tag_1 = models.CharField(max_length=20, choices=SKILLS, verbose_name=_("tag 1"))
-    tag_2 = models.CharField(max_length=20, choices=SKILLS, verbose_name=_("tag 2"))
-    tag_3 = models.CharField(max_length=20, choices=SKILLS, verbose_name=_("tag 3"))
-    tag_4 = models.CharField(max_length=20, choices=SKILLS, blank=True, null=True, verbose_name=_("tag 4"))
+    tag_1 = models.CharField(max_length=20, choices=SKILLS, verbose_name=_("spécialité 1"))
+    tag_2 = models.CharField(max_length=20, choices=SKILLS, verbose_name=_("spécialité 2"))
+    tag_3 = models.CharField(max_length=20, choices=SKILLS, verbose_name=_("spécialité 3"))
+    tag_4 = models.CharField(max_length=20, choices=SKILLS, blank=True, null=True, verbose_name=_("spécialité 4"))
+    tag_5 = models.CharField(max_length=20, choices=SKILLS, blank=True, null=True, verbose_name=_("spécialité 5"))
     # Per level
     hit_points_per_level = models.SmallIntegerField(default=0, verbose_name=_("points de santé par niveau"))
     skill_points_per_level = models.SmallIntegerField(default=0, verbose_name=_("points de compétence par niveau"))
@@ -173,16 +194,17 @@ class Character(models.Model):
         """
         Statistiques du personnage
         """
-        def __init__(self, character: Character):
+        def __init__(self, character):
             self.hit_points_per_level = character.hit_points_per_level + 3 + (character.endurance // 2)
             self.skill_points_per_level = character.skill_points_per_level + 5 + (2 * character.intelligence)
 
-            self.max_health = (character.max_health + 15 + (character.strength + (2 * character.endurance)) +
-                               ((character.level - 1) * self.hit_points_per_level))
+            self.max_health = (
+                character.max_health + 15 + (character.strength + (2 * character.endurance)) +
+                ((character.level - 1) * self.hit_points_per_level))
             self.max_action_points = character.max_action_points + 5 + (character.agility // 2)
             self.damage_resistance = character.damage_resistance
             self.damage_threshold = character.damage_threshold
-            self.armor_class = character.armor_class + character.agility
+            self.evade = character.armor_class + character.agility
             self.carry_weight = character.carry_weight + 25 + (25 * character.strength)
             self.melee_damage = character.melee_damage + max(0, character.strength - 5)
             self.poison_resistance = character.poison_resistance + 5 * character.endurance
@@ -198,7 +220,7 @@ class Character(models.Model):
             self.big_guns = character.big_guns + 2 * character.agility
             self.energy_weapons = character.energy_weapons + 2 * character.agility
             self.unarmed = character.unarmed + 30 + (2 * (character.strength + character.agility))
-            self.melee_damage = character.melee_damage + 20 + (2 * (character.strength + character.agility))
+            self.melee_weapons = character.melee_weapons + 20 + (2 * (character.strength + character.agility))
             self.throwing = character.throwing + 4 * character.agility
             self.first_aid = character.first_aid + 2 * (character.perception + character.endurance)
             self.doctor = character.doctor + 5 + character.perception + character.intelligence
@@ -214,18 +236,28 @@ class Character(models.Model):
             self.gambling = character.gambling + 5 * character.luck
             self.survival = character.survival + 2 * (character.endurance + character.intelligence)
 
-            for tag in [character.tag_1, character.tag_2, character.tag_3, character.tag_4]:
-                if not tag:
-                    continue
+            for tag in character.tags:
                 setattr(self, tag, getattr(self, tag, 0) + 20)
 
     @property
     def stats(self) -> Statistics:
-        return Character.Statistics(self)
+        stats = Character.STATISTICS.get(self.id) or Character.Statistics(self)
+        if self.id:
+            Character.STATISTICS[self.id] = stats
+        return stats
+
+    @property
+    def tags(self) -> List[str]:
+        return [tag for tag in [self.tag_1, self.tag_2, self.tag_3, self.tag_4, self.tag_5] if tag]
 
     def save(self, *args, **kwargs):
         self.check_level()
+        Character.STATISTICS.pop(self.id, None)
         super().save(*args, **kwargs)
+
+    def clean(self):
+        if len(self.tags) != len(set(self.tags)):
+            raise ValidationError(_("Une spécialisation ne peut être choisie plus d'une fois."))
 
     def check_level(self) -> int:
         """
@@ -244,3 +276,109 @@ class Character(models.Model):
             self.skill_points += self.stats.skill_points_per_level
             level += 1
         return level
+
+
+class Item(models.Model):
+    """
+    Objet
+    """
+    TYPE_WEAPON = 'weapon'
+    TYPE_AMMO = 'ammo'
+    TYPE_APPAREL = 'apparel'
+    TYPE_FOOD = 'food'
+    TYPE_CHEM = 'chem'
+    TYPE_MISC = 'misc'
+    TYPES = (
+        (TYPE_WEAPON, _("arme")),
+        (TYPE_AMMO, _("munition")),
+        (TYPE_APPAREL, _("vêtement")),
+        (TYPE_FOOD, _("nourriture")),
+        (TYPE_CHEM, _("drogue")),
+        (TYPE_MISC, _("autre")),
+    )
+
+    name = models.CharField(max_length=100, verbose_name=_("nom"))
+    image = models.ImageField(blank=True, null=True, verbose_name=_("image"))
+    type = models.CharField(max_length=7, choices=TYPES, verbose_name=_("type"))
+    value = models.PositiveIntegerField(default=0, verbose_name=_("valeur"))
+    weight = models.PositiveSmallIntegerField(default=0, verbose_name=_("poids"))
+    effects_wear = models.ManyToManyField('Effect', blank=True, related_name='+', verbose_name=_("effets quand porté"))
+    effects_use = models.ManyToManyField('Effect', blank=True, related_name='+', verbose_name=_("effets quand utilisé"))
+    effects_target = models.ManyToManyField('Effect', blank=True, related_name='+', verbose_name=_("effets sur cible"))
+
+    class Meta:
+        verbose_name = _("objet")
+        verbose_name_plural = _("objets")
+
+
+class Ammo(Item):
+    """
+    Munition
+    """
+    evasion_modifier = models.SmallIntegerField(default=0, verbose_name=_("modificateur d'esquive"))
+    damage_modifier = models.SmallIntegerField(default=0, verbose_name=_("modificateur de dégâts"))
+    resistance_modifier = models.SmallIntegerField(default=0, verbose_name=_("modificateur de résistance"))
+
+    class Meta:
+        verbose_name = _("munition")
+        verbose_name_plural = _("munitions")
+
+
+class Weapon(Item):
+    """
+    Arme
+    """
+    DAMAGE_NORMAL = 'normal'
+    DAMAGE_FIRE = 'fire'
+    DAMAGE_LASER = 'laser'
+    DAMAGE_PLASMA = 'plasma'
+    DAMAGE_EXPLOSIVE = 'explosive'
+    DAMAGE_TYPES = (
+        (DAMAGE_NORMAL, _("normal")),
+        (DAMAGE_FIRE, _("feu")),
+        (DAMAGE_LASER, _("laser")),
+        (DAMAGE_PLASMA, _("plasma")),
+        (DAMAGE_EXPLOSIVE, _("explosif")),
+    )
+
+    ammo = models.ManyToManyField('Ammo', blank=True, verbose_name=_("type de munition"))
+    damage_type = models.CharField(max_length=10, choices=DAMAGE_TYPES, verbose_name=_("type de dégâts"))
+    damage_dice_count = models.PositiveSmallIntegerField(default=0, verbose_name=_("nombre de dés"))
+    damage_dice_value = models.PositiveSmallIntegerField(default=0, verbose_name=_("valeur de dé"))
+    damage_bonus = models.PositiveSmallIntegerField(default=0, verbose_name=_("dégâts bonus"))
+    range = models.PositiveSmallIntegerField(blank=True, null=True, verbose_name=_("portée"))
+    clip_size = models.PositiveSmallIntegerField(blank=True, null=True, verbose_name=_("taille du chargeur"))
+    ap_cost_normal = models.PositiveSmallIntegerField(blank=True, null=True, verbose_name=_("coût PA normal"))
+    ap_cost_target = models.PositiveSmallIntegerField(blank=True, null=True, verbose_name=_("coût PA ciblé"))
+    ap_cost_burst = models.PositiveSmallIntegerField(blank=True, null=True, verbose_name=_("coût PA rafale"))
+    burst_count = models.PositiveSmallIntegerField(blank=True, null=True, verbose_name=_("munitions en rafale"))
+    min_stength = models.PositiveSmallIntegerField(blank=True, null=True, verbose_name=_("force minimum"))
+
+    class Meta:
+        verbose_name = _("arme")
+        verbose_name_plural = _("armes")
+
+
+class Apparel(Item):
+    """
+    Vêtement
+    """
+    evade = models.SmallIntegerField(default=0, verbose_name=_("esquive"))
+    resistance_normal = models.PositiveSmallIntegerField(default=0, verbose_name=_("résistance dégâts normaux"))
+    resistance_fire = models.PositiveSmallIntegerField(default=0, verbose_name=_("résistance dégâts feu"))
+    resistance_laser = models.PositiveSmallIntegerField(default=0, verbose_name=_("résistance dégâts laser"))
+    resistance_plasma = models.PositiveSmallIntegerField(default=0, verbose_name=_("résistance dégâts plasma"))
+    resistance_explosive = models.PositiveSmallIntegerField(default=0, verbose_name=_("résistance dégâts explosifs"))
+
+    class Meta:
+        verbose_name = _("vêtement")
+        verbose_name_plural = _("vêtements")
+
+
+class Effect(models.Model):
+    """
+    Effet
+    """
+    class Meta:
+        verbose_name = _("effet")
+        verbose_name_plural = _("effets")
