cfd6007f1b056200885123e808e7d1f3c6bd55f8
Marc <marc@debureaux.fr>
2020-01-01 05:58:57 +0100

    Patch for MultiSelectField support in DRF

--- fallout/patch.py
+++ fallout/patch.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+from rest_framework.serializers import ModelSerializer, MultipleChoiceField
 
 # Patch de django-multiselectfield pour compatibilité Django 2
 # https://github.com/goinnn/django-multiselectfield/issues/74
@@ -10,7 +11,17 @@ try:
             value = self.value_from_object(obj)
         return self.get_prep_value(value)
 
-    from multiselectfield.db import fields
-    fields.MultiSelectField.value_to_string = value_to_string
+    from multiselectfield.db.fields import MultiSelectField
+    MultiSelectField.value_to_string = value_to_string
+
+    # Patch des serializers DRF pour la gestion des champs MultiSelectField
+    def build_standard_field(self, field_name, model_field):
+        field_class, field_kwargs = self._build_standard_field(field_name, model_field)
+        if isinstance(model_field, MultiSelectField):
+            return MultipleChoiceField, field_kwargs
+        return field_class, field_kwargs
+
+    ModelSerializer._build_standard_field = ModelSerializer.build_standard_field
+    ModelSerializer.build_standard_field = build_standard_field
 except ImportError:
     pass
