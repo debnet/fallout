b07af5d97971aeb0feb8b08cd19e4e78dde131b1
Marc <marc@debnet.fr>
2022-01-20 13:41:34 +0100

    Update apps

--- fallout/apps.py
+++ fallout/apps.py
@@ -6,3 +6,4 @@ from django.utils.translation import gettext_lazy as _
 class FalloutConfig(AppConfig):
     name = 'fallout'
     verbose_name = _("Fallout RPG")
+    default_auto_field = 'django.db.models.AutoField'
